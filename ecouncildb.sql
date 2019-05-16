2b639e9c07e2f168a07685412fae77184e60a413
TheodorosKolokotronis <50631257+TheodorosKolokotronis@users.noreply.github.com>
2019-05-16 17:18:11 +0300

    Update ecouncildb.sql

--- ecouncildb.sql
+++ ecouncildb.sql
@@ -4,11 +4,17 @@ user_name   VARCHAR(30) NOT NULL,
 user_pass   VARCHAR(255) NOT NULL,
 user_email  VARCHAR(255) NOT NULL,
 user_date   DATETIME NOT NULL,
-user_level  INT(8) NOT NULL,
+user_level  ENUM('admin', 'student', 'professor', 'moderator'),
 UNIQUE INDEX user_name_unique (user_name),
+user_
 PRIMARY KEY (user_id)
 ) TYPE=INNODB;
 
+CREATE TABLE student (
+student_id     INT(8) NOT NULL REFERENCES users(id),
+user_level  ENUM('student') default 'student',
+PRIMARY KEY (student_id)
+) TYPE=INNODB;
 
 CREATE TABLE categories (
 cat_id          INT(8) NOT NULL AUTO_INCREMENT,
@@ -18,41 +24,78 @@ UNIQUE INDEX cat_name_unique (cat_name),
 PRIMARY KEY (cat_id)
 ) TYPE=INNODB;
 
-
-CREATE TABLE topics (
-topic_id        INT(8) NOT NULL AUTO_INCREMENT,
-topic_subject       VARCHAR(255) NOT NULL,
-topic_date      DATETIME NOT NULL,
-topic_cat       INT(8) NOT NULL,
-topic_by        INT(8) NOT NULL,
+CREATE TABLE subjects (
+subject_id        INT(8) NOT NULL AUTO_INCREMENT,
+subject_name       VARCHAR(255) NOT NULL,
+subject_date      DATETIME NOT NULL,
+subject_cat       INT(8) NOT NULL,
+subject_by        INT(8) NOT NULL,
+subject_session  INT(8) NOT NULL,
+subject_vote_count        INT(8) NOT NULL default 0,
 PRIMARY KEY (topic_id)
 ) TYPE=INNODB;
 
-
-CREATE TABLE posts (
-post_id         INT(8) NOT NULL AUTO_INCREMENT,
-post_content        TEXT NOT NULL,
-post_date       DATETIME NOT NULL,
-post_topic      INT(8) NOT NULL,
-post_by     INT(8) NOT NULL,
+CREATE TABLE comments (
+comment_id         INT(8) NOT NULL AUTO_INCREMENT,
+comment_content        TEXT NOT NULL,
+comment_date       DATETIME NOT NULL,
+comment_topic      INT(8) NOT NULL,
+comment_by     INT(8) NOT NULL,
 PRIMARY KEY (post_id)
 ) TYPE=INNODB;
+
+CREATE TABLE sessions (
+session_id         INT(8) NOT NULL AUTO_INCREMENT,
+start_date       DATETIME NOT NULL,
+end_date       DATETIME NOT NULL,
+activity numbers ENUM('0','1')
+PRIMARY KEY (post_id)
+) TYPE=INNODB;
+
+CREATE TABLE solutions (
+solution_id         INT(8) NOT NULL AUTO_INCREMENT,
+solution_content        TEXT NOT NULL,
+solution_date       DATETIME NOT NULL,
+solution_subject     INT(8) NOT NULL,
+solution_by     INT(8) NOT NULL,
+PRIMARY KEY (solution_id)
+) TYPE=INNODB;
+
+CREATE TABLE anouncements (
+anouncement_id         INT(8) NOT NULL AUTO_INCREMENT,
+announcements_content        TEXT NOT NULL,
+announcements_date       DATETIME NOT NULL,
+announcement_by     INT(8) NOT NULL,
+PRIMARY KEY (announcements_id)
+) TYPE=INNODB;
+
+CREATE TABLE social_hour_subject (
+sh_subject_id         INT(8) NOT NULL AUTO_INCREMENT,
+sh_subject_content        TEXT NOT NULL,
+sh_subject_date       DATETIME NOT NULL,
+announcement_by     INT(8) NOT NULL,
+sh_subject_vote_count        INT(8) NOT NULL default 0,
+PRIMARY KEY (announcements_id)
+) TYPE=INNODB;
+
+
+
 
 
 ALTER TABLE topics ADD FOREIGN KEY(topic_cat) REFERENCES categories(cat_id) ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE topics ADD FOREIGN KEY(topic_by) REFERENCES users(user_id) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE topics ADD FOREIGN KEY(topic_by) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE;
 
 ALTER TABLE posts ADD FOREIGN KEY(post_topic) REFERENCES topics(topic_id) ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE posts ADD FOREIGN KEY(post_by) REFERENCES users(user_id) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE posts ADD FOREIGN KEY(post_by) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE;
 
-INSERT INTO
-       users(user_name, user_pass, user_email ,user_date, user_level)
-VALUES('" . mysql_real_escape_string($_POST['user_name']) . "',
-       '" . sha1($_POST['user_pass']) . "',
-       '" . mysql_real_escape_string($_POST['user_email']) . "',
-       NOW(),   
-       0);
+ALTER TABLE topics ADD FOREIGN KEY(topic_session) REFERENCES sessions(session_id) ON DELETE CASCADE ON UPDATE CASCADE;
 
-       
+ALTER TABLE solutions ADD FOREIGN KEY(solution_subject) REFERENCES subject(subject_id) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE solutions ADD FOREIGN KEY(solution_by) REFERENCES student(student_id) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE announcements ADD FOREIGN KEY(announcement_by) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE social_hour_subject ADD FOREIGN KEY(announcement_by) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE;
