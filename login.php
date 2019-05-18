1449234c7386187a7dbed48c3e9ad6a9fa2b9c35
gkolovos <50678563+gkolovos@users.noreply.github.com>
2019-05-18 16:58:30 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,11 +1,12 @@
-<?php include('serv.php') ?>
+<?php include('//////serv.php') ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="styling.css">
   <title>E council</title>
 </head>
 <body >
@@ -13,29 +14,29 @@
   	<h2>Είσοδος</h2>
   </div>
 
-  <form method="post" action="  ///////login.php">
+  <form method="post" action=" login.php">
   	
-  	<div class="input-group">
+  	<div class="input">
   		<label>Email</label>
   		<input type="email" name="email" >
   	</div>
-  	<div class="input-group">
+  	<div class="input">
   		<label>Κωδικός</label>
   		<input type="password" name="password">
   	</div>
-  	<div class="input-group">
+  	<div class="input">
   		<button type="submit" class="btn" name="login_user">Είσοδος</button>
   	
-		////////</div>
+	</div>
   	
 	  
 	  <p>
-		  Δεν είστε xrhsths;&nbsp;  <a href="registration.php" style="color: #600000" ><u>Εγγραφή</u></a>
+		  Δεν είστε xrhsths;&nbsp;  <a href="////registration.php" style="color: #650000" ><u>Εγγραφή</u></a>
   	</p>
-	  ////////
+
   </form>
- 			&nbsp;&nbsp;&nbsp;&nbsp;<a href="loginm.php" style="color: #200000">Administrator</a> /
-			<a href="////////logind.php" style="color: #300000">Moderator</a>
+ 			&nbsp;&nbsp;&nbsp;&nbsp;<a href="/////loginm.php" style="color: #400000">Administrator</a> /
+			<a href="////////logind.php" style="color: #500000">Moderator</a>
 			
 
 </body>
