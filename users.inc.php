ebbf461c4ce27282a0274d569b554dd9e4a7b694
GeoSko <50631257+GeoSko@users.noreply.github.com>
2019-05-19 18:03:47 +0300

    Update users.inc.php

--- users.inc.php
+++ users.inc.php
@@ -35,7 +35,7 @@ class Student extends user {
       if(isset($_POST['save']))
          {
        $sql = "INSERT INTO subjects (subject_name,subject_date,subject_cat,subject_by,subject_description,subject_session)
-               VALUES ('".$_POST["subject_name"],GETDATE,'".$_POST["subject_cat"],this->id,'".$_POST["subject_description"],'".$_POST["subject_session"])";
+               VALUES ('".$_POST["subject_name"]."',GETDATE(),'".$_POST["subject_cat"]."',this->id,'".$_POST["subject_description"]."','".$_POST["subject_session"])."');
          }
     ?>
     <form method="post",action="writesubject.php"> 
@@ -65,6 +65,36 @@ class Student extends user {
   }
   function write_solution()
   {
+      header('location:write_subject.php')
+      
+    <?php
+      if(isset($_POST['save']))
+         {
+       $sql = "INSERT INTO comments (comment_content , comment_date, comment_subject, comment_by)
+               VALUES ('".$_POST["comment_content"]."',GETDATE,'".$_POST["comment_subject"]."',this->id,)";
+         }         
+    ?>
+    <form method="post",action="writesubject.php"> 
+    <subject_id="id"> subject_id:</id><br/>
+    <input type="text" subject_id="subject_id"><br/>
+    
+    <label for="subject_name">= subject_name:</label><br/>
+    <input type="text" subject_name="subject_name"><br/
+    
+    <label for="subject_cat">= subject_cat:</label><br/>
+    <input type="text" subject_cat="subject_cat"><br/
+    
+     <label for="subject_description">= subject_description:</label><br/>
+    <input type="text" subject_description="subject_descrition"><br/
+    
+     <label for="subject_session">= subject_session:</label><br/>
+    <input type="text" subject_session="subject_session"><br/
+   
+    <button type="submit" name="save">save</button>
+    
+    </form>
+</body>
+</html>
   }
   function confirm()
   {
