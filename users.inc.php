2b8a6b4bb82d83ac531af694df86f9861d8807e9
gkolovos <50678563+gkolovos@users.noreply.github.com>
2019-05-19 18:28:34 +0300

    Update users.inc.php

--- users.inc.php
+++ users.inc.php
@@ -35,11 +35,12 @@ class Student extends user {
       if(isset($_POST['save']))
          {
        $sql = "INSERT INTO subjects (subject_name,subject_date,subject_cat,subject_by,subject_description,subject_session)
-               VALUES ('".$_POST["subject_name"]."', GETDATE(), '".$_POST["subject_cat"]."' ,this->id,'".$_POST["subject_description"]."','".$_POST["subject_session"])."')";
+               VALUES ('".$_POST["subject_name"]."',GETDATE(),'".$_POST["subject_cat"]."',this->id,'".$_POST["subject_description"]."','".$_POST["subject_session"])."');
          }
     ?>
-
     <form method="post",action="writesubject.php"> 
+    <subject_id="id"> subject_id:</id><br/>
+    <input type="text" subject_id="subject_id"><br/>
     
     <label for="subject_name">= subject_name:</label><br/>
     <input type="text" subject_name="subject_name"><br/
@@ -59,27 +60,35 @@ class Student extends user {
 
 </body>
 </html>
-//αυτή η φόρμα κανονικα 'επρεπε να μπεί σε μια php αλλά δν έχει υλοποιήθεί
+///αυτή η φόρμα κανονικα 'επρεπε να μπεί σε μια php αλλά δν έχει υλοποιήθεί///// 
 
   }
   function write_solution()
   {
-      header('location:write_solution.php')
+      header('location:write_subject.php')
       
     <?php
       if(isset($_POST['save']))
          {
-       $sql = "INSERT INTO solutions (solution_content,solution_date,solution_subject,solution_by)
-               VALUES ('".$_POST["solution_content"]."',GETDATE(),'".$_POST["solution_subject"]."',this->id)";
-         }   
+       $sql = "INSERT INTO comments (comment_content , comment_date, comment_subject, comment_by)
+               VALUES ('".$_POST["comment_content"]."',GETDATE,'".$_POST["comment_subject"]."',this->id,)";
+         }         
     ?>
-    <form method="post",action="write_solution.php"> 
+    <form method="post",action="writesubject.php"> 
     <subject_id="id"> subject_id:</id><br/>
-    <input type="text" subject_id="solution_content"><br/>
+    <input type="text" subject_id="subject_id"><br/>
     
     <label for="subject_name">= subject_name:</label><br/>
-    <input type="text" subject_name="solution_subject"><br/
+    <input type="text" subject_name="subject_name"><br/
     
+    <label for="subject_cat">= subject_cat:</label><br/>
+    <input type="text" subject_cat="subject_cat"><br/
+    
+     <label for="subject_description">= subject_description:</label><br/>
+    <input type="text" subject_description="subject_descrition"><br/
+    
+     <label for="subject_session">= subject_session:</label><br/>
+    <input type="text" subject_session="subject_session"><br/
    
     <button type="submit" name="save">save</button>
     
@@ -93,7 +102,18 @@ class Student extends user {
   }
   function select_vote_subject()
   {
-
+{
+    $query = "SELECT * FROM subjects ";
+    $result = mysqli_query($ecouncildb, $query);
+      if (mysqli_num_rows($result) > 0) {
+          // output data of each row
+            while($row = mysqli_fetch_assoc($subjects)) {
+              echo ":session_id " . $row["session_id"].  "&nbsp&nbspSession&nbspName&nbsp" .$row['subject_name']. "<br>" ;
+              
+            }
+      } else {
+            echo "0 results";
+      }
   }
   function submit_vote()
   {
@@ -105,7 +125,7 @@ class Student extends user {
   }
   function select_solutions()
   {
-
+  
   }
   function insert_search_parameters()
   {
