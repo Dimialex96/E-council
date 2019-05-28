e1967cf4c234087181a6f099cec9f6060b4f6b33
Dimialex96 <dimi.alex@outlook.com>
2019-05-28 19:59:27 +0300

    update files

--- main_pages/index.php
+++ main_pages/index.php
@@ -1,6 +1,5 @@
 <?php
   session_start();
-
   if (!isset($_SESSION['email'])) {
   	$_SESSION['ecouncildb'] = "Πρέπει να συνδεθείτε πρώτα";
   	header('location: login.php');
@@ -11,6 +10,7 @@
   	header("location: login.php");
   }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,7 +19,7 @@
   <title>E council</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="/////styling.css">
-  <style> 
+  <style>
 body {
     background-image: url("images/evoting.jpg");
   min-height: 750px;
@@ -31,39 +31,32 @@ body {
 </style>
 </head>
 <body>
-  <br>
-  <h1 class="text-light">Ecouncil online<br>Forum and voting!</i></h1>
-  <br>
+  <br> <h1 class="text-light">Ecouncil online<br>Forum and voting!</i></h1> <br>
   <header>
-	      <div id="header-inner">
-		  <a href="index.php" id="logo"></a>
-		  <nav>
-		     <a href"#" id="menu-icon"></a>
-			 <ul>
-			     <li><a href="index.php" class="current">home</a></li>
-                 <li><a href="#">voting</a></li>
-                 <li><a href="#">forum</a></li>
-				 
-				</ul>
-			</nav>	
-		  </div>
+	   <div id="header-inner">
+		     <a href="index.php" id="logo"></a>
+		       <nav>
+		           <a href"#" id="menu-icon"></a>
+			         <ul>
+			              <li><a href="index.php" class="current">home</a></li>
+                    <li><a href="#">voting</a></li>
+                    <li><a href="#">forum</a></li>
+			         </ul>
+			     </nav>
+		 </div>
 	</header>
-     <br>  
-    <h1 class="text-white"> h psifos sou metra! </i></h1>
-     <br>         
-          
-    
+  <br> <h1 class="text-white"> h psifos sou metra! </i></h1> <br>
      <div class="logout1">
-  	<?php if (isset($_SESSION['success'])) : ?>
-      <div class="error success" >
-      	<h3>
-          <?php
-          	echo $_SESSION['success'];
-          	unset($_SESSION['success']);
-          ?>
-      	</h3>
-      </div>
-  	<?php endif ?></div>
+  	    <?php if (isset($_SESSION['success'])) : ?>
+          <div class="error success" >
+      	     <h3>
+               <?php
+          	    echo $_SESSION['success'];
+          	    unset($_SESSION['success']);
+               ?>
+      	     </h3>
+          </div>
+    </div>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
