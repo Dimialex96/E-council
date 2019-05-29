b7fb1f9f61cc46964d1566b18f17d44abb7eae36
Dimialex96 <50630907+Dimialex96@users.noreply.github.com>
2019-05-29 21:53:50 +0300

    Update loginadmin.php

--- main_pages/loginadmin.php
+++ main_pages/loginadmin.php
@@ -1,4 +1,4 @@
-<?php include('serveradmin.php') ?> //το αρχειο sereradmin.php δεν εχει υλοποιηθει
+<?php include('serveradmin.php') ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,29 +8,25 @@
   <link rel="stylesheet" type="text/css" href="styling.css">
   <title>E council</title>
 </head>
-    <style> 
-       body {
-         background-image: url("images/vote1.jpg");
-         min-height: 690px;
-         background-size: cover;
-         background-position: center;
-         background-repeat: no-repeat;
-         position: relative;}
-      </style>
-  	<h2>Είσοδος Administrator</h2>
-  <form method="post" action="loginadmin.php">	
+<body >
+  <div class="header">
+  	<h2>Είσοδος</h2>
+  </div>
+  <form method="post" action=" login.php">
   	<div class="input">
-  		<label>Username</label>
-  		<input type="text" name="user_name" >
+  		<label>Email</label>
+  		<input type="email" name="email" >
   	</div>
   	<div class="input">
   		<label>Κωδικός</label>
-  		<input type="user_pass" name="user_word">
+  		<input type="password" name="password">
   	</div>
-  	<div class="input-group">
-  		<button type="submit" class="btn" name="login_administrator">Είσοδος</button>
-  	</div>
+  	<div class="input">
+  		<button type="submit" class="btn" name="login_user">Είσοδος</button>
+	</div>
   </form>
-  </br> </br> </br> </br><h1><font color="white"><i>E council Online</br>Voting n Forum</i></font></h1>
+	<a href="login.php" style="color: #400000">Student</a>
+	<a href="loginmoderator.php" style="color: #500000">Moderator</a>
+	<a href="loginprofessor.php" style="color: #500000">Professor</a>
 </body>
 </html>
