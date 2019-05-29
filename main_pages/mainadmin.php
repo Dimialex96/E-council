46e0cc0664c28b454bded62a7a87e56e98d58761
Dimialex96 <50630907+Dimialex96@users.noreply.github.com>
2019-05-29 21:56:18 +0300

    Update mainadmin.php

--- main_pages/mainadmin.php
+++ main_pages/mainadmin.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-
   if (!isset($_SESSION['user_name'])) {
   	$_SESSION['ecouncildb'] = "Πρέπει να συνδεθείτε πρώτα";
   	header('location: login.php');
@@ -15,16 +14,13 @@ session_start();
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="styling.css">
-  
   <title>E council</title>
 </head>
-<body >
-    
+<body >  
          <div class="logout">
   	              <?php if (isset($_SESSION['success'])) : ?>
              <div class="error success" >
