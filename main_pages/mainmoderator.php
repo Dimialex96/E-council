0d2831450c70f20cdf67e990e5e7051d14d662bf
Dimialex96 <50630907+Dimialex96@users.noreply.github.com>
2019-05-29 21:59:15 +0300

    Update mainmoderator.php

--- main_pages/mainmoderator.php
+++ main_pages/mainmoderator.php
@@ -1,18 +1,26 @@
 <?php
+session_start();
+  if (!isset($_SESSION['user_name'])) {
+  	$_SESSION['ecouncildb'] = "Πρέπει να συνδεθείτε πρώτα";
+  	header('location: login.php');
+  }
+  if (isset($_GET['logout'])) {
+  	session_destroy();
+  	unset($_SESSION['user_name']);
+  	header("location: loginadmin.php");
+  }
+?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-  <link rel="stylesheet" type="text/css" href="styling.css">
-  
+  <link rel="stylesheet" type="text/css" href="styling.css">  
   <title>E council</title>
 </head>
-<body >
-    
+<body >  
          <div class="logout">
   	              <?php if (isset($_SESSION['success'])) : ?>
              <div class="error success" >
@@ -34,5 +42,4 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>
-
 ?>
